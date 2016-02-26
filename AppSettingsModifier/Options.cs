14517bf651e3a86a8fc0f22da942379b5e26de66
Eric Hansel <Eric Hansel>
2016-02-25 19:08:42 -0500

    Error pushing to remote. Hoping a commit fixes this.

---
+++ AppSettingsModifier/Options.cs
@@ -0,0 +1,46 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace AppSettingsModifier
+{
+    internal class Options
+    {
+        /// <summary>
+        /// Example "-a add" or "--action=add"
+        /// </summary>
+        [Option('a', "action", Required = true,
+          HelpText = "Action to perform on the new AppSettings value (Add).")]
+        public string Action { get; set; }
+
+        /// <summary>
+        /// Example "-k TestKey" or "--key=TestKey"
+        /// </summary>
+        [Option('k', "key", Required = true,
+          HelpText = "Key to add to the AppSettings node.")]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Example "-v TestValue" or "--value=TestValue"
+        /// </summary>
+        [Option('v', "value", Required = true,
+        HelpText = "Value to add to the AppSettings node.")]
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Example "-f "c:\project\web.config" or "--value="c:\project\web.config""
+        /// </summary>
+        [Option('f', "file", Required = true,
+        HelpText = "File that will have AppSettings node modified.")]
+        public string File { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ AppSettingsModifier/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace AppSettingsModifier
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DebugArgs(args);
+            var options = new Options();
+
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                XDocument xmlDoc = new XDocument();
+                try
+                {
+                    xmlDoc = XDocument.Load(options.File);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error loading file {0} {1}.", options.File, ex.Message);
+                    Environment.Exit((int)ExitCode.FileLoadException);
+                }
+
+                try
+                {
+                    var appSettingsNode = xmlDoc.Descendants("appSettings").First();
+                    if (options.Action.ToLower() == "add")
+                    {
+                        appSettingsNode.Add(new XElement("add", new XAttribute("key", options.Key), new XAttribute("value", options.Value)));
+                    }
+                    try
+                    {
+                        xmlDoc.Save(options.File);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error saving file {0} {1}.", options.File, ex.Message);
+                        Environment.Exit((int)ExitCode.FileSaveException);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error modifying XML {0}.", ex.Message);
+                    Environment.Exit((int)ExitCode.XmlModifyException);
+                }
+            }
+            else
+            {
+                Environment.Exit((int)ExitCode.InvalidArguments);
+            }
+
+            Environment.Exit((int)ExitCode.Success);
+        }
+
+        private static void DebugArgs(string[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                Debug.WriteLine("Arg {0}: {1}", i, args[i]);
+            }
+        }
+
+        private enum ExitCode : int
+        {
+            Success = 0,
+            InvalidArguments = 1,
+            FileLoadException = 2,
+            XmlModifyException = 3,
+            FileSaveException = 4
+        }
+    }
+}
